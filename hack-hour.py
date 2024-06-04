357f05ab72ff8bd7c9012988248b64198e3c4df9
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-06-04 17:09:36 -0400

    feat: add wifi connectivity

--- hack-hour.py
+++ hack-hour.py
@@ -1,12 +1,25 @@
-from machine import Pin
 import rp2
 import display
 
-# Main Code
+import secrets
+import network
+
 # make a loading animation where it goes ....0.1.2.3.4
 loadingAnimation = ["0000", "0001", "0012", "0123", "1234"]
-for item in loadingAnimation:
-    display.displayLED(item, 0.5)
+
+# Connect to the WiFi network
+wifi = network.WLAN(network.STA_IF)
+wifi.active(True)
+wifi.connect(secrets.SSID, secrets.PASSWORD)
+
+# Wait until the connection is established
+itemCounter = 0
+while not wifi.isconnected():
+    display.displayLED(loadingAnimation[itemCounter], 0.25)
+    itemCounter = (itemCounter + 1) % len(loadingAnimation)
+
+# Display the IP address
+display.displayLED(wifi.ifconfig()[0], 3)
 
 # wait until the button on the pico is pressed
 while rp2.bootsel_button() == 0:
