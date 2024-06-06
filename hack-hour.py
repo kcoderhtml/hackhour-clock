d855f1000156da1bd89a782f5fdd139f38ab7486
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-06-06 11:27:54 -0400

    feat: Sync time with server and display hack hour countdown

--- hack-hour.py
+++ hack-hour.py
@@ -1,7 +1,12 @@
 import display
+import time
+import socket
+import struct
+import machine
 
 import secrets
 import network
+import urequests
 
 # make a loading animation where it goes ....0.1.2.3.4
 loadingAnimation = ["0000", "0001", "0012", "0123", "1234"]
@@ -22,4 +27,44 @@ print(wifi.ifconfig()[0])
 
 display.displayLED("C00D", 0.5)
 
-# start listening for the hack hour from https://hackhour-9870d80cb898.herokuapp.com/
+
+def set_time():
+    NTP_QUERY = bytearray(48)
+    NTP_QUERY[0] = 0x1B
+    addr = socket.getaddrinfo("pool.ntp.org", 123)[0][-1]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.settimeout(1)
+        res = s.sendto(NTP_QUERY, addr)
+        msg = s.recv(48)
+    finally:
+        s.close()
+    val = struct.unpack("!I", msg[40:44])[0]
+    t = val - 2208988800
+    tm = time.gmtime(t)
+    machine.RTC().datetime(
+        (tm[0], tm[1], tm[2], tm[6] + 1, tm[3], tm[4], tm[5], 0))
+
+
+def getHackHourTime():
+    hackHourTime = urequests.get(
+        url="http://hackhour.hackclub.com/api/clock/U062UG485EE")
+
+    return hackHourTime.text
+
+
+# start listening for the hack hour
+# sync the time with the server
+print("Syncing time with server")
+# print local time
+print(time.localtime())
+set_time()
+# print synced time
+print(time.localtime())
+minutesLeft = 0
+while True:
+    # get the hack hour from the server
+    # find the minutes left as hack hour gives the end time in unix timestamp
+    minutesLeft = getHackHourTime()
+    print(minutesLeft)
+    display.displayLED(minutesLeft, 3)
