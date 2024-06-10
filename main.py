eb02d5d346d1919161e838c65e5f12babb3efc7e
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-06-10 17:05:17 -0400

    chore: Refactor displayLED function to handle timeToDisplay accurately

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def getHackHourTime():
         url="http://hackhour.hackclub.com/api/clock/U062UG485EE")
     return hackHourTime.text
 
+
 # start listening for the hack hour
 millisecondsLeft = 0
 while True:
@@ -15,10 +16,10 @@ while True:
     # find the minutes left as hack hour gives the end time in unix timestamp
     millisecondsLeft = getHackHourTime()
     minutesLeft = int(millisecondsLeft) / 1000 / 60
-    secondsLeftInThisMinute = (minutesLeft - (minutesLeft // 1)) * 60 + 1  # minutesLeft // 1 is the integer part of the minutes left
+    # minutesLeft // 1 is the integer part of the minutes left
+    secondsLeftInThisMinute = (minutesLeft - (minutesLeft // 1)) * 60 + 1
     print(f"{int(minutesLeft)}:{secondsLeftInThisMinute:02} remaining")
     if millisecondsLeft == "-1":
         display.displayLED("0000", 60)
     else:
-        display.displayLED(int(minutesLeft), # floor the minutes left
-                       secondsLeftInThisMinute)
+        display.displayLED(int(minutesLeft),  # floor the minutes left
